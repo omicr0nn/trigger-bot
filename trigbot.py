de6250e96795c7f4a56488a16fe9fec6ebf5d191
omicron <131537767+omicr0nn@users.noreply.github.com>
2023-09-01 16:21:50 +0300

    Update trigbot.py

--- trigbot.py
+++ trigbot.py
@@ -7,6 +7,34 @@ import winsound
 import os
 import mss
 from colorama import Fore, Style, init
+import sys
+import time
+from os import system
+
+system("cls||clear")
+def rainbow_text(text, delay=0.1):
+    colors = [
+    "\033[31m", "\033[33m", "\033[32m", "\033[36m", "\033[34m", "\033[35m", "\033[37m",
+    ]
+
+    for i in range(len(text)):
+        char = text[i]
+        color = colors[i % len(colors)]
+        sys.stdout.write(color + char)
+        sys.stdout.flush()
+        time.sleep(delay)
+
+    sys.stdout.write("\033[0m")
+    sys.stdout.write("\n")
+
+def main():
+    rainbow_text("Coding by omicr0n")
+    sys.stdout.write("\033[37m")
+    sys.stdout.write("Coding by Omicron\n")
+	
+if __name__ == "__main__":
+    rainbow_text("Coding by omicr0n")
+    
 S_HEIGHT, S_WIDTH = (PIL.ImageGrab.grab().size)
 PURPLE_R, PURPLE_G, PURPLE_B = (250, 100, 250)
 TOLERANCE = 75
